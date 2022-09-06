3509d5b12760d3ce4a1e9f31812d4476ec8ee873
lotsman-10 <leonidt@lotsman.org>
2022-09-06 15:08:16 +0500

    lesson 0

---
+++ lessons/Assets/scripts/elevator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("A");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("B");
+    }
+}
